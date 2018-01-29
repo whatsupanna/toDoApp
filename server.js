face0684529c7e554b519effd98cf7c09d283e1b
whatsupanna <annacmackenzie89@gmail.com>
2018-01-28 20:42:36 -0500

    progress commit adding features and styles

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const server = require('socket.io')();
 const firstTodos = require('./data');
 const Todo = require('./todo');
 
-//listen on a disconnect event, push into local storage
-// on reconnect send them to the server
-//then clear local storage
-
 
 // FIXME: DB is reloading on client refresh. It should be persistent on new client
 // connections from the last time the server was run...
+
+//moved this out of server connect so that it doesn't blow the database away every time. 
 
 const DB = firstTodos
             .map((t,i) => {
@@ -17,7 +15,6 @@ const DB = firstTodos
             })
             .map((t) => {
                 // Form new Todo objects
-                console.log(t);
                 return new Todo(t);
             });
 
@@ -35,7 +32,7 @@ server.on('connection', (client) => {
     }
 
     // Accepts when a client makes a new todo
-    client.on('make', (t) => {
+    client.on('add', (t) => {
         // Make a new todo
         const newTodo = new Todo(t);
         const newId = DB.length;
@@ -44,21 +41,62 @@ server.on('connection', (client) => {
         DB.push(newTodo);
 
         // Send the latest todos to the client
-        // FIXME: This sends all todos every time, could this be more efficient?
-        // reloadTodos();
+        // FIXME: This sends all todos every time, could this be more efficient? Yes fires event only for new to do. 
         console.log(newTodo);
         addNewTodo(newTodo);
     });
 
-    // let the user know that it is completed
+    // let the user know that a to do is completed
     client.on('completed', ({id}) => {
         DB[id].completed = true;
         server.emit('completedById', DB[id]);
     });
 
+    // let the user know all have been completed
+    client.on('completeAll', () => {
+        //complete all
+        completeAll();
+    });
+
+    // let the user know that a to do is deleted
+    client.on('deleted', ({id}) => {
+        //on deleted take it out 
+        deleteItem(id);
+    });
+
+    // let the user know all has been deleted
+    client.on('deleteAll', () => {
+        //delete all
+        deleteAll();
+    });
+
     // Send the DB downstream on connect
     reloadTodos();
+
+
+
+    // helper functions
+    function deleteItem(id){
+        DB.splice(DB[id].id,1);
+        DB[id].deleted = true;
+        server.emit('deletedById', DB[id]);
+    }
+
+    function completeAll(){
+        DB.forEach(function(item){
+            item.completed = true;
+        });
+        server.emit('completeAll', DB);
+    }
+
+    function deleteAll(){
+        for (let i = DB.length; i > 0; i--) {
+            DB.pop();
+        }
+        server.emit('deleteAll', DB);
+    }
 });
+
 
 console.log('Waiting for clients to connect');
 server.listen(3003);
--- todo.js
+++ todo.js
@@ -1,7 +1,8 @@
 module.exports = class Todo {
-    constructor({title, id, completed}) {
+    constructor({title, id, completed, deleted}) {
         this.title = title;
         this.id = id;
         this.completed = completed
+        this.deleted = deleted
     }
 }
